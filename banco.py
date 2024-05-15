a7e3981fac20afbf20bee28cef003af57d105dc7
Samuel Silva <samuuel.dsgn@gmail.com>
2024-05-14 21:02:47 -0300

    Adicionado funções

--- banco.py
+++ banco.py
@@ -3,6 +3,53 @@ import time
 def data_e_hora(operacao, valor):
     return (f'{time.strftime("%a, %d %m(%b) %y, %H : %M", time.localtime())} -- {operacao} - R${valor}')
 
+def deposito():
+    while True:
+            valor_deposito = input("Digite o valor a ser depositado: ")
+            valor_deposito = valor_deposito.replace(',', '.')
+
+            try:
+                valor_deposito = float(valor_deposito)
+                saldo_conta += valor_deposito
+                lista_extrato += [data_e_hora('Depósito', valor_deposito)]
+                break
+            
+            except ValueError:
+                print("Digite apenas números e [,] ou [.] Tente novamente")
+                continue
+
+def saque():
+    while True:
+        valor_saque = input('Digite o valor que será sacado: ')
+        valor_saque = valor_saque.replace(',', '.')
+
+        try:
+            valor_saque = float(valor_saque)
+
+            if valor_saque > saldo_conta:
+                print("Você não tem saldo suficiente. O saque não será efetuado")
+                break
+
+            elif limite_saque_atual >= LIMITE_SAQUE:
+                print("Você atingiu o limite diário máximo de saques. O saque não será efetuado")
+            
+            elif valor_saque > 500:
+                print("O limite máximo de saque é de R$500,00. O saque não será efetuado")
+
+            saldo_conta += (valor_saque * -1)
+            lista_extrato += [data_e_hora('Saque', valor_saque)]
+            limite_saque_atual += 1
+            break
+
+        except ValueError:
+            print("Digite apenas números e [,] ou [.] Tente novamente")
+            continue
+        
+def extrato():
+    for item in lista_extrato:
+        print (item, sep='\n')
+    print(f'Saldo Atual: R${saldo_conta:.2f}')
+        
 saldo_conta = 0.00
 lista_extrato = []
 limite_saque_atual = 0
@@ -19,54 +66,14 @@ while True:
     escolha = int(input(menu))
 
     if escolha == 1:
-
-        while True:
-
-            valor_deposito = input("Digite o valor a ser depositado: ")
-            valor_deposito = valor_deposito.replace(',', '.')
-
-            try:
-                valor_deposito = float(valor_deposito)
-                saldo_conta += valor_deposito
-                lista_extrato += [data_e_hora('Depósito', valor_deposito)]
-                break
-
-            except ValueError:
-                print("Digite apenas números e [,] ou [.] Tente novamente")
-                continue
-
+        deposito()
+        
     elif escolha == 2:
-            
-            while True:
-                valor_saque = input('Digite o valor que será sacado: ')
-                valor_saque = valor_saque.replace(',', '.')
-
-                try:
-                    valor_saque = float(valor_saque)
-
-                    if valor_saque > saldo_conta:
-                        print("Você não tem saldo suficiente. O saque não será efetuado")
-                        break
-
-                    elif limite_saque_atual >= LIMITE_SAQUE:
-                        print("Você atingiu o limite diário máximo de saques. O saque não será efetuado")
-                    
-                    elif valor_saque > 500:
-                        print("O limite máximo de saque é de R$500,00. O saque não será efetuado")
-
-                    saldo_conta += (valor_saque * -1)
-                    lista_extrato += [data_e_hora('Saque', valor_saque)]
-                    limite_saque_atual += 1
-                    break
-
-                except ValueError:
-                    print("Digite apenas números e [,] ou [.] Tente novamente")
-                    continue
+        saque()   
+         
 
     elif escolha == 3:
-        for item in lista_extrato:
-            print (item, sep='\n')
-        print(f'Saldo Atual: R${saldo_conta:.2f}')
+        extrato()
        
     elif escolha == 4:
         print ("SAINDO...")
