1edadc6971646d4fac9b571c1b56ca227f51c481
Samuel Silva <samuuel.dsgn@gmail.com>
2024-05-16 17:16:31 -0300

    Adicionado funções para criar novo usuário e nova conta

--- banco.py
+++ banco.py
@@ -4,12 +4,17 @@ saldo_conta = 0.00
 lista_extrato = []
 limite_saque_atual = 0
 LIMITE_SAQUE = 3
+AGENCIA =  '0001'
+contas = []
+usuarios = []
 
 menu = '''
 Digite [1] para DEPÓSITO
 Digite [2] para SAQUE
 Digite [3] para EXTRATO
-Digite [4] para SAIR
+Digite [4] para NOVO CLIENTE
+Digite [5] para NOVA CONTA
+Digite [6] para SAIR
 
 ==> '''
 
@@ -43,7 +48,38 @@ def extrato(saldo, /, *, extrato):
     for item in extrato:
         print (item, sep='\n')
     print(f'Saldo Atual: R${saldo:.2f}')
-        
+
+def criar_usuario(usuarios):
+    cpf = input('Informe seu CPF (Apenas números): ')
+    usuario_repetido = confere_usuario_existente(cpf, usuarios)
+
+    if usuario_repetido:
+        print('O CPF já se encontra cadastrado no sistema!')
+        return
+    
+    nome = input('Digite seu nome:')
+    data_nascimento = input('Digite sua data de nascimento (dd-mm-aaaa): ')
+    endereco = input('Digite seu endereço (logradouro, nº - bairro - cidade estado): ')
+    
+    usuarios.append ({'nome': nome, 'cpf': cpf, 'data_nascimento': data_nascimento, 'endereco': endereco})
+    print ('Usuário criado com sucesso!')
+
+def confere_usuario_existente (cpf, usuarios):
+    for usuario in usuarios:
+        if usuario['cpf'] == cpf:
+            return usuario['cpf']
+
+def criar_conta(usuarios, contas, agencia):
+    cpf = input('Digite seu CPF (Apenas números): ')
+    usuario_existente = confere_usuario_existente(cpf, usuarios)
+    if usuario_existente:
+        numero_conta = len(agencia) + 1
+        contas.append({'agencia': agencia, 'conta': numero_conta, 'usuario': usuario_existente})
+    else:
+        print ('CPF não cadastrado no sistema!')
+    
+    
+
 
 while True:
     escolha = int(input(menu))
@@ -76,8 +112,10 @@ while True:
         extrato(saldo_conta, extrato=lista_extrato)
        
     elif escolha == 4:
-        print ("SAINDO...")
-        break
+        criar_usuario(usuarios)
+
+    elif escolha == 5:
+        criar_conta(usuarios, contas, AGENCIA)
 
     else:
         print('Por favor, digite um valor válido. Tente novamente')
